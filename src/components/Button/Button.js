4eb0e6ca8034a6a7ceddf8652bb21a8f9bc79341
Dimachello <korovind195@gmail.com>
2021-01-09 12:46:44 +0200

    add some rxjs code

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,30 +1,39 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import { fromEvent } from "rxjs";
 
 const MyButton = ({ actionId, text, action, limitation }) => {
   let Button = null;
+  const btnRef = useRef(null);
+  let actionName = null;
 
   switch (actionId) {
-    case 1: //onClick
-      Button = (
-        <button className="btn" onClick={() => action()}>
-          {text}
-        </button>
-      );
+    case 1:
+      actionName = "click";
       break;
-    case 2: //onDoubleClick
-      Button = (
-        <button
-          className="btn"
-          onDoubleClick={() => action()}
-          disabled={limitation}
-        >
-          {text}
-        </button>
-      );
+    case 2:
+      actionName = "dblclick";
       break;
     default:
-      return Button;
+      actionName = "click";
   }
+
+  Button = (
+    <button
+      type="button"
+      ref={btnRef}
+      className="btn"
+      disabled={limitation ? limitation : false}
+    >
+      {text}
+    </button>
+  );
+
+  useEffect(() => {
+    const clickable = fromEvent(btnRef.current, actionName).subscribe(() =>
+      action()
+    );
+    return () => clickable.unsubscribe();
+  }, [actionName, action]);
 
   return Button;
 };
